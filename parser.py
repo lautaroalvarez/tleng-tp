a4fd4adf5a98d3147eef744d8590cd9f15b3191d
Lautaro Alvarez <lautarolalvarez@gmail.com>
2018-06-26 23:09:38 -0300

    Subo una forma de encararlo, pasando estados entre los padres e hijos (variante a atributos heredados)

--- parser.py
+++ parser.py
@@ -1,12 +1,17 @@
+from __future__ import print_function
 import ply.yacc as yacc
 from lexer import tokens
 
-indentation = 0
+status = {
+    'indentation': 0,
+    'type': '',
+    'parentType': '',
+    'first': False,
+    'initial': True
+}
 
 def p_initial(p):
     'initial : value'
-    p[0]=p[1]
-        
 
 def p_value_lasttype(p):
     '''value : STRING
@@ -15,86 +20,133 @@ def p_value_lasttype(p):
              | FALSE
              | NULL
     '''
+    global status
+    if status['type'] == 'array':
+        if not status['first']:
+            printIndentation()
+        print("- ",end='')
     print(p[1])
-    p[0] = p[1]
 
 def p_value_object(p):
-    'value : object'
-    p[0] = p[1]
-    
+    'value : saveStatusBeforeObject object'
+    global status
+    status = p[1]
+
 def p_value_array(p):
-    'value : array'
-    p[0] = p[1]
+    'value : saveStatusBeforeArray array'
+    global status
+    status = p[1]
 
 def p_object_empty(p):
     'object : LLLAVE RLLAVE'
     print('{}')
-    p[0] = "objeto vacio"
 
 def p_object_members(p):
-    'object : LLLAVE  members  RLLAVE'
-    p[0] = p[2]    
+    'object : LLLAVE printBeforeObjectNotEmpty members RLLAVE'
 
 def p_members_one(p):
     'members : pair'
-    p[0] = p[1]
 
 def p_members_multiple(p):
     'members : pair COMA members'
-    p[0] = p[1]
 
 def p_pair(p):
     'pair : STRING printString DOSPUNTOS value'
-    p[0] = p[4]
 
 def p_array_empty(p):
     'array : LCORCHETE RCORCHETE'
     print('[]')
-    p[0] = "lista vacia"
 
 def p_array_elements(p):
-    'array : LCORCHETE indentationP elements RCORCHETE indentationM'
-    p[0] = p[3]
+    'array : LCORCHETE printBeforeArrayNotEmpty elements RCORCHETE'
 
 def p_elements_one(p):
-    'elements : printV value'
-    p[0] = p[1]
+    'elements : value'
 
 def p_elements_multiple(p):
-    'elements : printV value COMA elements'
-    p[0] = p[1]
+    'elements : value COMA setNotFirst elements'
 
 
 def p_printString(p):
     'printString :'
-    string =p[-1]
-    if string[0]!="-": 
-        string = string.replace('"', '')     
-    print(string+":",end='')
+    global status
+    string = p[-1]
+    if string[0] != '-':
+        string = string.replace('"', '')
+    if not status['first']:
+        printIndentation()
+    print(string+": ",end='')
+    status['first'] = False
 
-def p_indentation(p):
-    'indentation :'
-    
-def p_indentationP(p):
-    'indentationP :'
-    global indentation
-    indentation = indentation +1
-    print()
+def p_saveStatusBeforeObject(p):
+    'saveStatusBeforeObject :'
+    global status
+    p[0] = {
+        'indentation': status['indentation'],
+        'type': status['type'],
+        'parentType': status['parentType'],
+        'first': status['first'],
+        'initial': status['initial']
+    }
+    if not status['initial'] and not status['type'] == 'array':
+        status['indentation'] += 1
+    if status['type'] == 'array':
+        printIndentation()
+        print('- ', end='')
+    status['first'] = True
+    status['parentType'] = status['type']
+    status['type'] = 'object'
+    status['initial'] = False
 
-def p_indentationM(p):
-    'indentationM :'
-    global indentation
-    indentation = indentation -1
+def p_printBeforeObjectNotEmpty(p):
+    'printBeforeObjectNotEmpty :'
+    if status['parentType'] != 'array':
+        print('')
+        printIndentation()
 
-def p_printV(p):
-    'printV :'
-    for i in range(indentation*2):
-        print(" ",end='')
+def p_saveStatusBeforeArray(p):
+    'saveStatusBeforeArray :'
+    global status
+    p[0] = {
+        'indentation': status['indentation'],
+        'type': status['type'],
+        'parentType': status['parentType'],
+        'first': status['first'],
+        'initial': status['initial']
+    }
+    if status['type'] == 'array':
+        if not status['first']:
+            printIndentation()
+        print('- ', end='')
+    status['parentType'] = status['type']
+    status['type'] = 'array'
+    status['initial'] = False
+
+def p_printBeforeArrayNotEmpty(p):
+    'printBeforeArrayNotEmpty :'
+    if status['parentType'] != 'array':
+        print('')
+        status['first'] = False
+    else:
+        status['first'] = True
+    status['indentation'] += 1
+
+def p_setNotFirst(p):
+    'setNotFirst :'
+    global status
+    status['first'] = False
 
 def p_error(p):
     print("Error de sintaxis!")
     exit(1)
 
+def printIndentation():
+    global status
+    indentation = status['indentation']
+    if status['type'] == 'array':
+        indentation -= 1
+    for i in range(indentation * 2):
+        print(" ",end='')
 
 
 parser = yacc.yacc()
@@ -107,6 +159,50 @@ parser = yacc.yacc()
 #    if not s: continue
 #    result = parser.parse(s)
 #    print(result)
-s = '{"a" :1,"c":[1,2,3],"b":[3,4,{ "n":[2,3,3]},3]}'
-result = parser.parse(s)
-print(result)
+
+test1 = '''
+{
+    "a": [
+        1,
+        {
+            "b": 2,
+            "c": "asd",
+            "d": []
+        },
+        [
+            [3],
+            4,
+            [
+                5,
+                6,
+                [
+                    [7]
+                ]
+            ]
+        ],
+        8
+    ]
+}'''
+
+test2 = '''
+{
+  "a" :1,
+  "c":[1,2,3],
+  "b":[3,4,{ "n":[2,3,3,{"o":{"p":"fgh"}}],"m":"a"},3],
+  "d": {
+    "a": 1,
+    "b": "asd"
+  }
+}
+'''
+
+test3 = '''
+["asd",0,[],{"a":{},"b":1},[1,2]]
+'''
+
+print('JSON:')
+print(test3)
+print('')
+print('YAML:')
+result = parser.parse(test3)
+print('')
